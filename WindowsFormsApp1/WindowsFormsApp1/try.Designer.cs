877a96db2f63a0964537b04edfcb5650bf5b0812
峻宇 李 <jimmy2333060@gmail.com>
2021-07-13 19:55:54 +0800

    修改message

--- WindowsFormsApp1/WindowsFormsApp1/try.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/try.Designer.cs
@@ -36,7 +36,7 @@ namespace WindowsFormsApp1
             // 
             this.button1.Location = new System.Drawing.Point(195, 165);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(101, 34);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
